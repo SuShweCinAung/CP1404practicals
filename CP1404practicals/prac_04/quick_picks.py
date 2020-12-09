7383cef7f39883605c4cb801d6d5a21051a5f455
SuShweCinAung <sushwecinaung234@gmail.com>
2020-12-09 21:40:47 +0630

    modify code to display randomly pick numbers with ascending orders

--- CP1404practicals/prac_04/quick_picks.py
+++ CP1404practicals/prac_04/quick_picks.py
@@ -1,1 +1,13 @@
-import random from randint
+import random
+def main():
+    number_list=[]
+    turn = int(input("How many quick picks? "))
+    for i in range(1, turn+1):
+        for num in range(0, 6):
+            num = random.randint(1,45)
+            while num in number_list:
+                num = random.randint(1, 45)
+            number_list.append(num)
+            number_list.sort()
+        print("{:>2} {:>2} {:>2} {:>2} {:>2} {:>2}".format(number_list[0],number_list[1],number_list[2],number_list[3],number_list[4],number_list[5]))
+main()
