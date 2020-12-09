f2f087c76bd67af87f273d4eb8350261bcbb2ef0
SuShweCinAung <sushwecinaung234@gmail.com>
2020-12-09 19:46:07 +0630

    modify code to display subject details

--- CP1404practicals/prac_04/subject_reader.py
+++ CP1404practicals/prac_04/subject_reader.py
@@ -5,25 +5,29 @@ Data file -> lists program
 
 FILENAME = "subject_data.txt"
 
-
 def main():
     data = get_data()
     print(data)
-
+    display_details()
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data_list = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        repr(line)
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data_list.append(parts)
     input_file.close()
+    return data_list
 
+def display_details():
+    input_file = open(FILENAME)
+    for line in input_file:
+        parts = line.split(',')  # Separate the data into its parts
+        parts[2] = int(parts[2])
+        print("{} is taught by {:12} and has {:>3} students".format(parts[0], parts[1], parts[2]))
 
 main()
