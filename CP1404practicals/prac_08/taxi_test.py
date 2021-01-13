4baf3c8354b83b9771979c318f4e9432b1146ac9
SuShweCinAung <sushwecinaung234@gmail.com>
2021-01-13 18:51:02 +0630

    fix the parameter

--- CP1404practicals/prac_08/taxi_test.py
+++ CP1404practicals/prac_08/taxi_test.py
@@ -1,7 +1,7 @@
 from prac_08.taxi import Taxi
 
 def main():
-    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi = Taxi("Prius 1", 100)
     new_taxi.drive(40)
     print(new_taxi)
     new_taxi.start_fare()
